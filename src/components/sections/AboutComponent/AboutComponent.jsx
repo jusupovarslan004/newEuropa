79eaa33f057bad3381de86ee2091776d9f65e0b8
Arslan Jusupov <jusupovarslan04@gmail.com>
2024-12-13 02:44:11 +0600

    create bekend about me

--- src/components/sections/AboutComponent/AboutComponent.jsx
+++ src/components/sections/AboutComponent/AboutComponent.jsx
@@ -1,34 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import "./AboutComponent.scss";
 
 const About = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const [aboutData, setAboutData] = useState([]);
 
-  const aboutItems = [
-    {
-      title: t("about.items.1.title"),
-      description: t("about.items.1.description"),
-    },
-    {
-      title: t("about.items.2.title"),
-      description: t("about.items.2.description"),
-    },
-    {
-      title: t("about.items.3.title"),
-      description: t("about.items.3.description"),
-    },
-  ];
+  useEffect(() => {
+    const currentLang = i18n.language === "kg" ? "ky" : i18n.language;
+
+    fetch(
+      `https://api.togetherrecruitment.kg/api/v2/info/about_us/?lang=${currentLang}`
+    )
+      .then((response) => response.json())
+      .then((data) => setAboutData(data))
+      .catch((error) => console.error("Error fetching about data:", error));
+  }, [i18n.language]);
 
   return (
     <div className="about">
       <div className="container">
         <h2 className="about__title">{t("about.title")}</h2>
         <div className="about__grid">
-          {aboutItems.map((item, index) => (
-            <div key={`about-item-${index}`} className="about__item">
-              <h3 className="about__item-title">{item.title}</h3>
-              <p className="about__description">{item.description}</p>
+          {aboutData.map((item, index) => (
+            <div
+              key={`about-item-${index}`}
+              className={`about__item ${
+                index === 1 ? "text-first" : "image-first"
+              }`}
+            >
+              {index === 1 ? (
+                <>
+                  <p className="about__description">{item.text}</p>
+                  <img
+                    src={item.photo_api}
+                    alt={`About ${index + 1}`}
+                    className="about__image"
+                  />
+                </>
+              ) : (
+                <>
+                  <img
+                    src={item.photo_api}
+                    alt={`About ${index + 1}`}
+                    className="about__image"
+                  />
+                  <p className="about__description">{item.text}</p>
+                </>
+              )}
             </div>
           ))}
         </div>
